93d1604ecd202dbbde9e38f01e1c277a9815f45f
Jhonatan Arevalo <jhon20339@gmail.com>
2025-01-18 19:54:35 -0500

    Backend: Modificación de createTask Controller

--- Backend/src/controllers/createTask.controller.js
+++ Backend/src/controllers/createTask.controller.js
@@ -1,29 +1,29 @@
 import { writeFile, readFile } from '../utils/file.js'
 
-export const createTask = async (req, res) => {
+export const createTask = async (request, response) => {
   try {
     const currentDate = new Date()
     const formatedDate = `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear()}`
 
-    const nuevoDato = {
-      id: req.body.id,
-      title: req.body.title,
-      description: req.body.description,
+    const nuevaTarea = {
+      id: request.body.id,
+      title: request.body.title,
+      description: request.body.description,
       createdAt: formatedDate,
     }
 
     let database = await readFile()
     const maxId = database.reduce((max, item) => (item.id > max ? item.id : max), 0)
-    nuevoDato.id = maxId + 1
-    database.push(nuevoDato)
+    nuevaTarea.id = maxId + 1
+    database.push(nuevaTarea)
 
     const completed = await writeFile(JSON.stringify(database))
     if (completed) {
-      res.status(200).json({ message: 'Tarea agregada con éxito' })
+      response.status(200).json({ message: 'Tarea agregada con éxito' })
     } else {
-      return res.status(409).json({ message: 'No fue posible crear la tarea' })
+      return response.status(409).json({ message: 'No fue posible crear la tarea' })
     }
   } catch (error) {
-    return res.status(500).json({ message: error.message })
+    return response.status(500).json({ message: error.message })
   }
 }
